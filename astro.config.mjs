c17f17b0b079f6365694fc274e88b553a12ed29a
Rui Yokoyama <r.yokoyama@cirfect-tech.com>
2023-10-06 11:07:49 +0900

    a
    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
 
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,5 @@ export default defineConfig({
   buildOptions: {
     site: "https://r-yokoyama-cft.github.io/",
     base: "https://github.com/r-yokoyama-cft/2310_gtm_sample"
-  },
+  }
 });
